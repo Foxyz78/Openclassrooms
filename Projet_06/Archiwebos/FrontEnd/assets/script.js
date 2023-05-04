26be01e42363716772aca2f4ff4c983af7967316
Anthony lamart <foxyz78@gmail.com>
2023-05-04 17:25:13 +0200

    "Étape 1.1 : Récupération des travaux depuis le back-end"

--- Projet_06/Archiwebos/FrontEnd/assets/script.js
+++ Projet_06/Archiwebos/FrontEnd/assets/script.js
@@ -1,15 +1,15 @@
 // Permet d'ajouter et d'afficher les images dynamiquement
 function add_work() {
     for (i = 0; i <= 5; i++) {
-        let works = i;
+        let work = i;
         const figure = document.createElement("figure")
         fetch("http://localhost:5678/api/works")
             .then(response => response.json())
             .then(data => {
                 const img = document.createElement("img");
                 const text = document.createElement("figcaption");
-                img.src = data[works].imageUrl;
-                text.innerHTML = data[works].title;
+                img.src = data[work].imageUrl;
+                text.innerHTML = data[work].title;
                 figure.appendChild(img);
                 figure.appendChild(text);
                 document.querySelector(".gallery").appendChild(figure);
