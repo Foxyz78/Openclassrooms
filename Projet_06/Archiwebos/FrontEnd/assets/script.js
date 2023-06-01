2d4fb9331b12e686e95b47d542644915a52003a9
Foxyz78 <foxyz78@gmail.com>
2023-06-01 15:55:05 +0200

    Etape 3.3 : Envoi du projet

--- Projet_06/Archiwebos/FrontEnd/assets/script.js
+++ Projet_06/Archiwebos/FrontEnd/assets/script.js
@@ -1,26 +1,44 @@
 // Requête fetch pour récuperer les travaux sur l'api
-const works = await fetch("http://localhost:5678/api/works").then(works => works.json());
+const works = await fetch("http://localhost:5678/api/works").then((works) =>
+  works.json()
+);
+
+// Requête fetch pour récuperer les catégories sur l'api
+const categories = await fetch("http://localhost:5678/api/categories").then(
+  (categories) => categories.json()
+);
+
+const body = document.querySelector("body");
+const arrow_left = document.querySelector(".fa-arrow-left");
+const btn_modal = document.querySelector(".modal__button");
+const gallery = document.querySelector(".modal__gallery");
+const add_image = document.querySelector(".box-add-image");
+
+const modal__works = document.querySelector(".modal__works");
+const modal__add_image = document.querySelector(".modal__add-image");
 
 // Génére la page dynamiquement avec les balises HTML
 function get_work(works) {
-    const nb_work = works.length;
-    if (document.querySelector(".gallery")) {
+  const nb_work = works.length;
+  if (document.querySelector(".gallery")) {
+    for (let i = 0; i < nb_work; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const text = document.createElement("figcaption");
+      const trash = document.createElement("i");
+      img.src = works[i].imageUrl;
 
-        for (let i = 0; i < nb_work; i++) {
-            const figure = document.createElement("figure")
-            const img = document.createElement("img");
-            const text = document.createElement("figcaption");
-
-            img.src = works[i].imageUrl;
-            text.innerHTML = works[i].title;
-            figure.appendChild(img);
-            figure.appendChild(text);
-            document.querySelector(".gallery").appendChild(figure);
-        }
+      text.innerHTML = works[i].title;
+      figure.appendChild(img);
+      trash.classList.add("fa-solid", "fa-trash-can");
+      figure.appendChild(trash);
+      figure.appendChild(text);
+      document.querySelector(".gallery").appendChild(figure);
     }
+  }
 }
 
-// Affichage de la page
+// Affichage des images
 get_work(works);
 
 // Gestion des boutons pour les filtres
@@ -31,104 +49,163 @@ const btn_appartement = document.querySelector(".btn-appartement");
 
 // Réinitialise la gallerie en supprimant les balises enfants la classe ".gallery"
 function reset_gallery() {
-    document.querySelector(".gallery").innerHTML = "";
+  document.querySelector(".gallery").innerHTML = "";
 }
 
 btn_reset.addEventListener("click", function () {
-    const filter_all = works.filter(function (work) {
-        return work.categoryId > 0 && work.categoryId < 4;
-    })
-    reset_gallery();
-    get_work(filter_all);
+  const filter_all = works.filter(function (work) {
+    return work.categoryId > 0 && work.categoryId < 4;
+  });
+  reset_gallery();
+  get_work(filter_all);
 });
 
 btn_objet.addEventListener("click", function () {
-    const filter_objet = works.filter(function (work) {
-        return work.categoryId == 1;
-    })
-    reset_gallery();
-    get_work(filter_objet);
+  const filter_objet = works.filter(function (work) {
+    return work.categoryId == 1;
+  });
+  reset_gallery();
+  get_work(filter_objet);
 });
 
 btn_appartement.addEventListener("click", function () {
-    const filter_appartement = works.filter(function (work) {
-        return work.categoryId == 2;
-    })
-    reset_gallery();
-    get_work(filter_appartement);
+  const filter_appartement = works.filter(function (work) {
+    return work.categoryId == 2;
+  });
+  reset_gallery();
+  get_work(filter_appartement);
 });
 
 btn_hotel.addEventListener("click", function () {
-    const filter_hotel = works.filter(function (work) {
-        return work.categoryId == 3;
-    })
-    reset_gallery();
-    get_work(filter_hotel);
+  const filter_hotel = works.filter(function (work) {
+    return work.categoryId == 3;
+  });
+  reset_gallery();
+  get_work(filter_hotel);
 });
 
-// Création de la modal
+// Création de la fenêtre de dialogue
 function open_modal(works) {
-    const nb_work = works.length;
-    if (document.querySelector(".modal__gallery")) {
-        for (let i = 0; i < nb_work; i++) {
-            const figure = document.createElement("figure")
-            const img = document.createElement("img");
+  const nb_work = works.length;
+  modal__add_image.style.display = "none";
+  if (document.querySelector(".modal__gallery")) {
+    // Boucle permettant de créer la gallerie
+    for (let i = 0; i < nb_work; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const text = document.createElement("span");
+      const trash = document.createElement("i");
+      const div = document.createElement("div");
 
-            const text = document.createElement("span");
+      // Ajoute l'id sur les images afin de les cibler pour la suppression
+      img.setAttribute("id", "imageId" + works[i].id);
 
-            img.src = works[i].imageUrl;
-            text.innerHTML = "Editer";
-            figure.appendChild(img);
-            figure.appendChild(text);
-            document.querySelector(".modal__gallery").appendChild(figure);
-        }
+      img.src = works[i].imageUrl;
+      text.innerHTML = "Editer";
+
+      div.classList.add("delete-image");
+      figure.appendChild(div);
+
+      // Affichage de de l'icone poubelle sur l'image
+      trash.classList.add("fa-solid", "fa-trash-can", "fa-2xs");
+      figure.appendChild(img);
+      div.appendChild(trash);
+      figure.appendChild(text);
+      document.querySelector(".modal__gallery").appendChild(figure);
     }
+    const suppression = document.querySelectorAll(".delete-image");
+
+    suppression.forEach(function (element) {
+      element.addEventListener("click", function (e) {
+        delete_image();
+      });
+    });
+  }
 }
 
-function close_modal() {
-    let btn_close = document.querySelector(".close");
-    btn_close.addEventListener("click", function () {
-        document.querySelector("#modal").style.display = "none";
-    })
+function delete_image() {
+  // Todo: récupérer l'id de l'image à supprimer
+  alert("test de la suppression");
 }
+
+btn_modal.addEventListener("click", function () {
+  modal__add_image.style.display = "flex";
+  modal__works.style.display = "none";
+});
+
+// Affiche les catégories enregistrées sur l'api dans la liste déroulante
+function display_categories() {
+  for (let i = 0; i < categories.length; i++) {
+    const input_category = document.querySelector("select");
+    const option = document.createElement("option");
+    input_category.appendChild(option);
+    option.innerHTML = categories[i].name;
+  }
+}
+
+display_categories();
+
+// Ferme la fenêtre modale
+function close_modal() {
+  let btn_close = document.querySelectorAll(".close");
+  btn_close.forEach((e) => {
+    e.addEventListener("click", function () {
+      document.querySelector("#modal").style.display = "none";
+      body.style.backgroundColor = "#FFFEF8";
+    });
+  });
+}
+
 open_modal(works);
 close_modal();
 
 const tokenId = window.localStorage.getItem("1");
-// Sélection de du lien Login/logout dans la barre de navigation
-let log = document.querySelector("nav li:nth-child(3)")
+// Sélection du lien Login/logout dans la barre de navigation
+let log = document.querySelector("nav li:nth-child(3)");
 
 if (tokenId) {
-    log.innerHTML = "logout";
-    //document.querySelector(".mode-edition").style.display = "flex";
-    display_edtion_mode();
+  log.innerHTML = "logout";
+  //document.querySelector(".mode-edition").style.display = "flex";
+  display_edtion_mode();
 }
 
-// permet de changer la page d'acccueil en mode édition
+// Change la page d'acccueil en mode édition
 function display_edtion_mode() {
-    //Affichage du bouton modifier avec l'icone
-    let edition = document.querySelectorAll(".mode-edition");
-    edition.forEach(e => {
-        e.style.display = "flex";
-    })
-    // Suppression du filtre pour les projets
-    document.querySelector(".portfolio__filtre").style.display = "none";
-    document.querySelector("header").style.margin = "83px 0"
+  //Affichage du bouton modifier avec l'icone
+  let edition = document.querySelectorAll(".mode-edition");
+  edition.forEach((e) => {
+    e.style.display = "flex";
+  });
+
+  // Suppression du filtre pour les projets
+  document.querySelector(".portfolio__filtre").style.display = "none";
+  document.querySelector("header").style.margin = "83px 0";
 }
 
 // Cible la div "edition__modification" qui contient l'icone et texte modifier
 let btn__modifier = document.querySelector(".edition__modification");
 
 btn__modifier.addEventListener("click", function () {
-    document.querySelector("#modal").style.display = "block";
-})
+  // Affiche la fenêtre modale
+  document.querySelector("#modal").style.display = "block";
+  // Met l'opacité du body à 30% lorsque la fenêtre modale apparaît
+  body.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+  modal__add_image.style.display = "none";
+  modal__works.style.display = "flex";
+});
 
 function log_out() {
-    log.addEventListener("click", function () {
-        // suppression du token et redirection
-        window.localStorage.removeItem("1");
-        window.location.href = "index.html";
-    });
+  log.addEventListener("click", function () {
+    // suppression du token et redirection
+    window.localStorage.removeItem("1");
+    window.location.href = "index.html";
+  });
 }
+
+// Affichage de la fenêtre de dialogue précédente
+arrow_left.addEventListener("click", function () {
+  modal__works.style.display = "flex";
+  modal__add_image.style.display = "none";
+});
 
 log_out();
