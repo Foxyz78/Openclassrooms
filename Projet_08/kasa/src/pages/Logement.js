fd6ea53bd51e52f3e513ea6820691d684905c7f9
Foxyz78 <foxyz78@gmail.com>
2023-08-29 12:40:14 +0200

    "Projet fini"

--- Projet_08/kasa/src/pages/Logement.js
+++ Projet_08/kasa/src/pages/Logement.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Navbar from "../components/Header";
@@ -32,10 +32,11 @@ const Logement = () => {
     return stars;
   };
 
-  // usestate pour le slide du carrousel
+  // Usestate pour le slide du carrousel
   const [slide, setSlide] = useState(0);
   const [indicator, setIndicator] = useState(1);
 
+  // Fonctions permettant de faire défilier les inmages
   const slideToRight = () => {
     setSlide(slide === numberImages - 1 ? 0 : slide + 1);
     setIndicator(indicator === numberImages ? 1 : indicator + 1);
@@ -44,6 +45,10 @@ const Logement = () => {
     setSlide(slide === 0 ? numberImages - 1 : slide - 1);
     setIndicator(indicator === 1 ? numberImages : indicator - 1);
   };
+
+  // Constantes pour affaicher et cacher les fleches du carrousel
+  const displayFlex = { display: "flex" };
+  const displayNone = { display: "none" };
 
   return (
     <>
@@ -54,6 +59,7 @@ const Logement = () => {
           className="arrow arrow-left"
           src="../images/arrow-left.png"
           alt="flêche de gauche"
+          style={numberImages > 1 ? displayFlex : displayNone}
         />
         {propsData.pictures.map((pictures, index) => {
           return (
@@ -70,9 +76,11 @@ const Logement = () => {
           className="arrow arrow-right"
           src="../images/arrow-right.png"
           alt="flêche de droite"
+          style={numberImages > 1 ? displayFlex : displayNone}
         />
         <p>
-          {indicator}/{numberImages}
+          {/* Affiche l'indicateur du nombre d'image et le cache s'il n'y a qu'une seule image */}
+          {numberImages > 1 ? indicator + "/" + numberImages : null}
         </p>
       </div>
 
