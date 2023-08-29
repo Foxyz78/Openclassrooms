5c8a3b384a5301423c47fa760d9c5aa168215a83
Foxyz78 <foxyz78@gmail.com>
2023-08-29 15:10:14 +0200

    "projet fini"

--- Projet_08/kasa/src/pages/Logement.js
+++ Projet_08/kasa/src/pages/Logement.js
@@ -23,6 +23,7 @@ const Logement = () => {
     for (let i = 0; i < rating; i++) {
       stars.push(
         <FontAwesomeIcon
+          key={i}
           icon="fa-solid fa-star"
           size="xl"
           style={{ color: "#ff6060" }}
